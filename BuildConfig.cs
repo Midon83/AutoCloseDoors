ee0a6da6d262384b6f387aa6cd93d9de4ee2f746
Chris <chrisloa@t-online.de>
2024-06-05 11:08:08 +0200

    Removed unused stuff

--- BuildConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AutoCloseDoors.Config
-{
-    public class BuildConfig
-    {
-        public const string PackageID = "id.igame.autoclosedoor";
-        public const string Name = "AutoCloseDoors";
-        public const string Version = "2.0.0";
-    }
-}
